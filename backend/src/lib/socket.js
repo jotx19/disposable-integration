54bcd057e5f34dd4257e9a70f5cd66917568dbab
Prabjot Singh <prabjot24x7@gmail.com>
2025-09-24 04:33:37 -0400

    try

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000"
+            , process.env.FRONTEND_URL || ""
+        ],
     }
 });
 
