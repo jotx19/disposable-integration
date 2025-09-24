744f6b3064bf82cbc9f2d1f5a0422c47116203c1
Prabjot Singh <prabjot24x7@gmail.com>
2025-09-24 03:10:09 -0400

    dark_mode

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 const Layout = async ({ children }: Props) => {
   return (
-    <ThemeProvider attribute="class" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <div className="flex flex-col min-h-screen">{children}
         <Header/>
       </div>
