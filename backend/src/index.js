255f19e452718c8be3716f1c59f45147d66c3ff7
Prabjot Singh <prabjot24x7@gmail.com>
2025-09-24 03:45:53 -0400

    production-backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(express.json({limit: "50mb"}));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://disposable.vercel.app"],
   credentials: true, 
 }));
 
