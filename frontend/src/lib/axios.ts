6d07ba463bf692220c6c5f6a90607acf398f913d
Prabjot Singh <prabjot24x7@gmail.com>
2025-09-26 15:34:38 -0400

    frontend_jwt_improvements

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -6,6 +6,16 @@ export const BASE_URL =
     : "http://localhost:5001";
 
 export const axiosInstance = axios.create({
-    baseURL: `${BASE_URL}/api`,
-    withCredentials: true,
+  baseURL: `${BASE_URL}/api`,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt"); 
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
